85e5793e9657fa69eca95b8368f864327947441f
Eduardo Mendes <z4r4tu5tr4@users.noreply.github.com>
2016-04-15 14:25:59 -0300

    Update conjugaime.py

--- conjugaime.py
+++ conjugaime.py
@@ -7,37 +7,49 @@ a = prim_conj(palavra)
 print(a.presente())
 """
 class prim_conj:
-    
+    """
+    Classe da primera cojugação, responsável pelos verbos terminados em ar do indicativo
+    """
     def __init__(self, verbo):
         if verbo[-2:] != "ar":
             print("Este verbo não pertence a primera conjugação")
         else:
             self.verbo = verbo
+            self.pessoas = ["eu", "tu", "ela/ele", "nós", "vós", "elas/eles"]
 
     def presente(self):
-        """ Recebe um verbo regular e retorna um dicionário com o verbo conjugado"""
-
-        conjugado = OrderedDict()
         irregulares = {}
-
         irregulares["dar"] = ["dou", "dás", "dá", "damos", "dais", " 	dão"]
         irregulares["estar"] = ["estou","estás","está","estamos","estais","estão"]
         irregulares["passear"] = ["passeio","passeias","passeia","passeamos", "passeais","passeiam"]
-        irregulares["aboloar"] = ["abaúlo", "abaúlas", "abaúla", "abaulamos", "abaulais", "abaúlam"]
-        irregulares["adequar"] = ["adéquo", "adéquas", "adéqua", "adequamos", "adequais", "adéquam"]
+        irregulares["averiguar"] = ["averíguo","averíguas","averígua","averiguamos", "averiguais","averíguam"]
 
-        pessoas = ["eu", "tu", "ela/ele", "nós", "vós", "elas/eles"]
 
+        sufixos = ["o", "as", "a", "amos", "ais", "am"]
+
+        return self.resposta(irregulares,sufixos)
+
+    def pret_per(self):
+        irregulares = {}
+        irregulares["dar"] = ["dei","deste","deu","demos","destes","deram"]
+        irregulares["estar"] =  ["estive", "estiveste", "esteve", "estivemos", "estivestes", "estiveram",]
+        irregulares["passear"] = ["passeei", "passeaste", "passeou", "passeamos", "passeastes", "passearam"]
+
+        sufixos = [	"ei", "aste", "ou", "ámos", "astes", "aram"]
+
+        return self.resposta(irregulares,sufixos)
+
+    def resposta(self, irregulares, sufixos):
+        conjugado = OrderedDict()
         if self.verbo not in irregulares:
 
-            sufixos = ["o", "as", "a", "amos", "ais", "am"]
             radical = self.verbo[:-2]
 
-            for pessoa, sufixo in zip(pessoas, sufixos):
+            for pessoa, sufixo in zip(self.pessoas, sufixos):
                 conjugado[pessoa] = "{radical}{sufixo}".format(radical=radical,sufixo=sufixo)
 
         else:
-            for pessoa, verbo in zip(pessoas, irregulares[self.verbo]):
+            for pessoa, verbo in zip(self.pessoas, irregulares[self.verbo]):
                 conjugado[pessoa] = verbo
 
         return conjugado
